dfad145e9fc0f0afd7ae501b9dd49e363c32b75a
Christian Stangier <christian@stangier.email>
2020-05-18 11:55:53 +0200

    Initial setup

---
+++ CucumberSwiftExample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CucumberSwiftExample
+//
+//  Created by Christian Stangier on 18.05.20.
+//  Copyright © 2020 Christian Stangier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+platform :macos, '10.14'
+
+target 'CucumberSwiftExample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CucumberSwiftExample
+
+  target 'CucumberSwiftExampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'CucumberSwift'
+  end
+
+end
